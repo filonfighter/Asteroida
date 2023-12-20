f09a66c7aeb579a3a78fe75d0ddcd46b23b6b46a
filonfighter <cameraatliberty@gmail.com>
2023-12-20 19:30:52 +0100

    Initial commit

---
+++ Esp32backToBasics/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  delay(1000);
+}
+
+// put function definitions here:
