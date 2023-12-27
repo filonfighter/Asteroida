3e123a7c71914ea4aa887096a4e98212d129a4f0
filonfighter <cameraatliberty@gmail.com>
2023-12-27 17:29:23 +0100

    modified the code to work with the new motor driver

--- Esp32BasicMotorControl/src/main.cpp
+++ Esp32BasicMotorControl/src/main.cpp
@@ -3,16 +3,16 @@
 // Following code works so when the button is pressed the start begins opening Oxygen and Fuel valves
 // After specified time the valves close and the test is over
 // If the button is pressed during the test, the valves close and the test is over
+// This is made specifically for the BTS7960B motor driver
 
 // Motor Control Pins
-int motorOxygenEN = 34; // Motor Oxygen Enable Pin (must be PWM)
-int motorOxygenIN1 = 32; // Motor Oxygen Input 1 Pin
-int motorOxygenIN2 = 33; // Motor Oxygen Input 2 Pin
+int motorOxygenCW_EN = 34; // Motor Oxygen Enable Pin
+int motorOxygenCCW_EN = 33; // Motor Oxygen Enable Pin
+int motorOxygenSpeed = 32; // Motor Oxygen Speed input (must be PWM)
 
-int motorFuelEN = 35; // Motor Fuel Enable Pin (must be PWM)
-int motorFuelIN1 = 25; // Motor Fuel Input 1 Pin
-int motorFuelIN2 = 26; // Motor Fuel Input 2 Pin
-
+int motorFuelCW_EN = 35; // Motor Fuel Enable Pin
+int motorFuelCCW_EN = 25; // Motor Fuel Enable Pin
+int motorFuelSpeed = 36; // Motor Fuel Speed input (must be PWM)
 // Button Pins
 int buttonStart = 27; // Start Button Pin
 
@@ -20,30 +20,31 @@ int buttonStart = 27; // Start Button Pin
 unsigned long waitTime = 5000; // 5 second
 
 // Motor Open/Close time in miliseconds
-unsigned long motorTime = 1000; // 1 second
+unsigned long motorOpeningTime = 1000; // 1 second
+unsigned long motorClosingTime = 2000; // 2 seconds
 
-// Motor retrival speed
-int motorSpeedRetrive = 200; // 200/255
+// Motor closing speed
+int motorSpeedClose = 200; // 200/255
 
 // Motor opening speed
 int motorSpeedOpen = 255; // 255/255
 
 // Forward declarations
-void MotorOxygenForward(int speed);
-void MotorOxygenBackward(int speed);
+void MotorOxygenOpen(int speed);
+void MotorOxygenClose(int speed);
 void MotorOxygenStop();
-void MotorFuelForward(int speed);
-void MotorFuelBackward(int speed);
+void MotorFuelOpen(int speed);
+void MotorFuelClose(int speed);
 void MotorFuelStop();
 
 // Pin Setup
 void setup() {
-  pinMode(motorOxygenEN, OUTPUT);
-  pinMode(motorOxygenIN1, OUTPUT);
-  pinMode(motorOxygenIN2, OUTPUT);
-  pinMode(motorFuelEN, OUTPUT);
-  pinMode(motorFuelIN1, OUTPUT);
-  pinMode(motorFuelIN2, OUTPUT);
+  pinMode(motorOxygenCW_EN, OUTPUT);
+  pinMode(motorOxygenCCW_EN, OUTPUT);
+  pinMode(motorOxygenSpeed, OUTPUT);
+  pinMode(motorFuelCW_EN, OUTPUT);
+  pinMode(motorFuelCCW_EN, OUTPUT);
+  pinMode(motorFuelSpeed, OUTPUT);
   pinMode(buttonStart, INPUT);
 }
 
@@ -51,9 +52,9 @@ void setup() {
 void loop() {
   if (digitalRead(buttonStart) == HIGH) {
     // Open valves
-    MotorOxygenForward(motorSpeedOpen);
-    MotorFuelForward(motorSpeedOpen);
-    delay(motorTime); // Added missing semicolon
+    MotorOxygenOpen(motorSpeedOpen);
+    MotorFuelOpen(motorSpeedOpen);
+    delay(motorOpeningTime);
     MotorOxygenStop();
     MotorFuelStop();
 
@@ -62,53 +63,57 @@ void loop() {
     while (millis() - startTime < waitTime) {
       // If button is pressed, stop test
       if (digitalRead(buttonStart) == LOW) {
-        MotorOxygenBackward(motorSpeedRetrive);
-        MotorFuelBackward(motorSpeedRetrive);
-        // Wait for fuse to burn
+        MotorOxygenClose(motorSpeedClose);
+        MotorFuelClose(motorSpeedClose);
+        delay(motorClosingTime);
+        MotorOxygenStop();
+        MotorFuelStop();
         return;
       }
     }
 
     // Close valves
-    MotorOxygenBackward(motorSpeedRetrive);
-    MotorFuelBackward(motorSpeedRetrive);
-    // Wait for fuse to burn
+    MotorOxygenClose(motorSpeedClose);
+    MotorFuelClose(motorSpeedClose);
+    delay(motorClosingTime);
+    MotorOxygenStop();
+    MotorFuelStop();
   }
 }
 
 // Motor Control Functions
-void MotorOxygenForward(int speed) { // Added type for parameter
-  analogWrite(motorOxygenEN, speed); // Set speed
-  digitalWrite(motorOxygenIN1, HIGH);
-  digitalWrite(motorOxygenIN2, LOW);
+void MotorOxygenOpen(int speed) {
+  analogWrite(motorOxygenSpeed, speed); // Set speed
+  digitalWrite(motorOxygenCW_EN, HIGH);
+  digitalWrite(motorOxygenCCW_EN, LOW);
 }
 
-void MotorOxygenBackward(int speed) { // Added type for parameter
-  analogWrite(motorOxygenEN, speed); // Set speed
-  digitalWrite(motorOxygenIN1, LOW);
-  digitalWrite(motorOxygenIN2, HIGH);
+void MotorOxygenClose(int speed) {
+  analogWrite(motorOxygenSpeed, speed); // Set speed
+  digitalWrite(motorOxygenCW_EN, LOW);
+  digitalWrite(motorOxygenCCW_EN, HIGH);
 }
 
 void MotorOxygenStop() {
-  analogWrite(motorOxygenEN, 0); // Set speed to 0%
-  digitalWrite(motorOxygenIN1, LOW);
-  digitalWrite(motorOxygenIN2, LOW);
+  analogWrite(motorOxygenSpeed, 0); // Set speed to 0%
+  digitalWrite(motorOxygenCW_EN, LOW);
+  digitalWrite(motorOxygenCCW_EN, LOW);
 }
 
-void MotorFuelForward(int speed) { // Added type for parameter
-  analogWrite(motorFuelEN, speed); // Set speed
-  digitalWrite(motorFuelIN1, HIGH);
-  digitalWrite(motorFuelIN2, LOW);
+void MotorFuelOpen(int speed) {
+  analogWrite(motorFuelSpeed, speed); // Set speed
+  digitalWrite(motorFuelCW_EN, HIGH);
+  digitalWrite(motorFuelCCW_EN, LOW);
 }
 
-void MotorFuelBackward(int speed) { // Added type for parameter
-  analogWrite(motorFuelEN, speed); // Set speed
-  digitalWrite(motorFuelIN1, LOW);
-  digitalWrite(motorFuelIN2, HIGH);
+void MotorFuelClose(int speed) {
+  analogWrite(motorFuelSpeed, speed); // Set speed
+  digitalWrite(motorFuelCW_EN, LOW);
+  digitalWrite(motorFuelCCW_EN, HIGH);
 }
 
 void MotorFuelStop() {
-  analogWrite(motorFuelEN, 0); // Set speed to 0%
-  digitalWrite(motorFuelIN1, LOW);
-  digitalWrite(motorFuelIN2, LOW);
+  analogWrite(motorFuelSpeed, 0); // Set speed to 0%
+  digitalWrite(motorFuelCW_EN, LOW);
+  digitalWrite(motorFuelCCW_EN, LOW);
 }
