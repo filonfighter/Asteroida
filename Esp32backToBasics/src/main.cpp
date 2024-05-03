bd996f37201db6d154f72c878d115c22cbe313db
filonfighter <cameraatliberty@gmail.com>
2024-05-03 13:50:01 +0200

    asds

--- Esp32backToBasics/src/main.cpp
+++ Esp32backToBasics/src/main.cpp
@@ -1,12 +1,60 @@
 #include <Arduino.h>
 
-// Poniższy program powstał do testowania ogólnej sprawności komuinikacji między komputerem a płytką ESP32
+// Motor Control Pins
+int motorOxygenCW_EN = 39; // Motor Oxygen Enable Pin
+int motorOxygenCCW_EN = 36; // Motor Oxygen Enable Pin
+int motorOxygenSpeed = 34; // Motor Oxygen Speed input (must be PWM)
 
+// Motor Open/Close time in miliseconds
+unsigned long motorOxygenOpeningTime = 5000; // 5 seconds
+unsigned long motorOxygenClosingTime = 5000; // 5 seconds
+
+// Motor closing speed
+int motorSpeedClose = 200; // 200/255
+
+// Motor opening speed
+int motorSpeedOpen = 255; // 255/255
+
+// Forward declarations
+void MotorOxygenOpen(int speed);
+void MotorOxygenClose(int speed);
+void MotorOxygenStop();
+
+// Pin Setup
 void setup() {
-  Serial.begin(9600);
+  pinMode(motorOxygenCW_EN, OUTPUT);
+  pinMode(motorOxygenCCW_EN, OUTPUT);
+  pinMode(motorOxygenSpeed, OUTPUT);
 }
 
+// Main Loop
 void loop() {
-  Serial.println("Hello World");
-  delay(1000);
+  // Open valve
+  MotorOxygenOpen(motorSpeedOpen);
+  delay(motorOxygenOpeningTime);
+  MotorOxygenStop();
+
+  // Close valve
+  MotorOxygenClose(motorSpeedClose);
+  delay(motorOxygenClosingTime);
+  MotorOxygenStop();
+}
+
+// Motor Control Functions
+void MotorOxygenOpen(int speed) {
+  analogWrite(motorOxygenSpeed, speed); // Set speed
+  digitalWrite(motorOxygenCW_EN, HIGH);
+  digitalWrite(motorOxygenCCW_EN, LOW);
+}
+
+void MotorOxygenClose(int speed) {
+  analogWrite(motorOxygenSpeed, speed); // Set speed
+  digitalWrite(motorOxygenCW_EN, LOW);
+  digitalWrite(motorOxygenCCW_EN, HIGH);
+}
+
+void MotorOxygenStop() {
+  analogWrite(motorOxygenSpeed, 0); // Set speed to 0%
+  digitalWrite(motorOxygenCW_EN, LOW);
+  digitalWrite(motorOxygenCCW_EN, LOW);
 }
